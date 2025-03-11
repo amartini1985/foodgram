a881faa5ae7f6169e148f6573dabc9ea2751b996
Andrey Martyanov <martyanovav@yandex.ru>
2025-03-11 17:48:41 +0300

    Second

--- backend/api/management/commands/load_csv_data.py
+++ backend/api/management/commands/load_csv_data.py
@@ -1,6 +1,7 @@
 """Модуль для загрузки csv файлов!."""
 import csv
 import os
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError

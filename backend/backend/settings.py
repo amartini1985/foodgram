b5822b951f70681a5f9b36c5e4f1c282c890cfc8
Andrey Martyanov <martyanovav@yandex.ru>
2025-03-12 13:14:26 +0300

    Fix bags

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,9 +2,9 @@ import os
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent

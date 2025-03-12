37781255cf78d6749ad07a5f23d8fb9b8bdfb2be
Andrey Martyanov <martyanovav@yandex.ru>
2025-03-12 17:12:09 +0300

    Fix bags

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         # Меняем настройку Django: теперь для работы будет использоваться
@@ -85,6 +85,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
